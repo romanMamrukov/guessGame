3ea1accf6acf328dee445daca7d6a1e60f83586b
romanMamrukov <roman.mamrukov@gmail.com>
2026-03-17 23:00:31 +0200

    Updates

--- backend/src/db/database.ts
+++
@@ -1,9 +0,0 @@
-const Database = require("better-sqlite3");
-import type { Database as BetterSqlite3Database } from "better-sqlite3";
-import path from "path";
-
-const dbPath = path.join(__dirname, "../../../database/game.db");
-
-const db: BetterSqlite3Database = new Database(dbPath);
-
-export default db;
