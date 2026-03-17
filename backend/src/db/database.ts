f42f44434b832495bd631e046931481df61b7d74
romanMamrukov <roman.mamrukov@gmail.com>
2026-03-17 13:53:19 -0700

    fix: remove legacy sqlite files to fix build

--- backend/src/db/database.ts
+++
@@ -1,8 +0,0 @@
-import Database from "better-sqlite3";
-import path from "path";
-
-const dbPath = path.join(__dirname, "../../../database/game.db");
-
-const db = new Database(dbPath);
-
-export default db;
